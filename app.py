f057a2964df8d0e1aa4c1fcd8735991ebec01ebe
CuongNguyen25190410 <cuongdz12234566@gmail.com>
2026-04-16 22:59:11 +0700

    update new

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
+print("App is starting...")
 import pickle
+from flask import Flask, request, jsonify, render_template, redirect, url_for
+import numpy as np
+import pandas as pd
+app = Flask(__name__)
+# load the model
+model = pickle.load(open('regmodel.pkl', 'rb'))
+
+@app.route('/') 
+def home():
+    return render_template('home.html')
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data=request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    scalar = pickle.load(open('scaling.pkl', 'rb'))
+    new_data = scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    # output=regmodel.predict(new_data)
+    output = model.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+if __name__ == "__main__":
+    app.run(debug=True)
